60700a00d3dcd2cc2da56a025d22b0e596ca1a76
Tuan-AnhTran <140397297+Tuan-AnhTran@users.noreply.github.com>
2025-01-11 16:53:40 +0100

    add documentation for strsplit1

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,15 @@
+#' Title
+#'
+#' @param x A charactrer vector with one element
+#' @param split what to split
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' x = 'alpha,beta,charlie,delta'
+#' strsplit1(x, ',')
+#'
 strsplit1 = function(x, split){
   strsplit(x, split = split)[[1]]
 }
